2c48d73e816e7a00dd15dd7cfc907c4c6bcf13b6
Yanpin Chouli <a131234568@gmail.com>
2024-05-03 15:31:48 +0800

    feat: build web basic layout

--- shop_web/urls.py
+++ shop_web/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from shop_web.views import shop_views
 
 urlpatterns = [
-    path('', shop_views.index_view),
+    path('', shop_views.shop_page, name='shop_page'),
 ]
--- shop_web/views/shop_views.py
+++ shop_web/views/shop_views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def index_view(request):
-    return render(request, 'index.html')
+def shop_page(request):
+    return render(request, 'base.html')
