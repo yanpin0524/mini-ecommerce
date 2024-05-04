e7b8fb1b5e2a15464edd0b5ba821b654646d064d
Yanpin Chouli <a131234568@gmail.com>
2024-05-04 21:45:53 +0800

    ✨ feat(shop.models): Add default image in Product Model's image field

--- shop/models.py
+++ shop/models.py
@@ -47,7 +47,9 @@ class Product(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
-    image = models.ImageField(upload_to='products', null=False, blank=False)
+    image = models.ImageField(
+        upload_to='products', null=False, blank=False, default='products/product_default.png'
+    )
 
     def __str__(self):
         return self.name
