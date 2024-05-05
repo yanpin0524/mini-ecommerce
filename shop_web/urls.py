6771bbbe5b742e7d614147fefe8526536d1e6a5d
Yanpin Chouli <a131234568@gmail.com>
2024-05-05 21:30:06 +0800

    ✨ feat(view): 新增 product_detail 的 view

--- shop_web/urls.py
+++ shop_web/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from shop_web.views import shop_views
 
 urlpatterns = [
-    path('', shop_views.shop_page, name='shop_page'),
+    path('product/<int:product_id>/', shop_views.product_detail, name='product_detail'),
+    path('shop/', shop_views.shop_page, name='shop_page'),
+    path('', shop_views.shop_page),
 ]
--- shop_web/views/shop_views.py
+++ shop_web/views/shop_views.py
@@ -5,5 +5,10 @@ from shop.models import Product
 
 def shop_page(request):
     products = Product.objects.all()
-
     return render(request, 'shop.html', {'products': products})
+
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+
+    return render(request, 'product_detail.html', {'product': product})
