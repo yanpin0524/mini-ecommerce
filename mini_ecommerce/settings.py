d49a2f2564ab824184ded47cbf6fa5bb93c0d92b
Yanpin Chouli <a131234568@gmail.com>
2024-05-07 18:22:43 +0800

    ✨ feat(template): 新增客制化的 404 page not found 頁面

--- mini_ecommerce/settings.py
+++ mini_ecommerce/settings.py
@@ -66,7 +66,9 @@ ROOT_URLCONF = 'mini_ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
