45afe34d4decdfb319daa853192454f82711c8b8
Yanpin Chouli <a131234568@gmail.com>
2024-05-07 18:26:21 +0800

    🧪 test: 新增 shop, product_detail 頁面的測試

---
+++ shop_web/tests/test_urls.py
@@ -0,0 +1,24 @@
+from django.test import Client, TestCase
+from faker import Faker
+
+from shop.models import Product
+
+
+class TestShopUrls(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.faker = Faker()
+
+        self.product = Product.objects.create(
+            name=self.faker.text(70),
+            price=self.faker.pyfloat(3, 2, 1, True),
+            description=self.faker.text(200),
+        )
+
+    def test_shop_page(self):
+        response = self.client.get('/shop/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_detail_page(self):
+        response = self.client.get(f'/product/{self.product.id}/')
+        self.assertEqual(response.status_code, 200)
