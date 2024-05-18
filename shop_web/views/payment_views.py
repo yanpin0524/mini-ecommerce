d9e075b8d2d12a5904978d41777c5c6345b971c4
Yanpin Chouli <a131234568@gmail.com>
2024-05-18 22:16:25 +0800

    ✨ feat(checkout): 嘗試串接綠界金流

---
+++ shop_web/views/payment_views.py
@@ -0,0 +1,72 @@
+import os
+
+from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from dotenv import load_dotenv
+
+from shop_web.services.ecpay_service import ECPayAllInOne
+
+load_dotenv()
+
+
+class Checkout(View):
+    def get(self, request):
+        ecpay = ECPayAllInOne(
+            MerchantID=os.getenv('MERCHANTID'),
+            HashKey=os.getenv('HASHKEY'),
+            HashIV=os.getenv('HASHIV'),
+            ReturnURL=reverse('checkout-return'),
+            ClientBackURL=reverse('checkout-client-back'),
+        )
+
+        return HttpResponse(
+            ecpay.create_order(TradeDesc='測試交易', ItemName='我是測試用商品', TotalAmount=150)
+        )
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CheckoutReturn(View):
+    def post(self, request):
+        print(request.POST)
+
+        ecpay = ECPayAllInOne(
+            MerchantID=os.getenv('ECPAY_MERCHANT_ID'),
+            HashKey=os.getenv('ECPAY_HASH_KEY'),
+            HashIV=os.getenv('ECPAY_HASH_IV'),
+        ).ecpay_payment_sdk
+
+        res = request.POST.dict()
+        back_check_mac_value = request.POST.get('CheckMacValue')
+        check_mac_value = ecpay.generate_check_value(res)
+
+        if back_check_mac_value == check_mac_value:
+            return HttpResponse('1|OK')
+
+        return HttpResponse('0|Fail')
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CheckoutClientBack(View):
+    def post(self, request):
+        ecpay = ECPayAllInOne(
+            MerchantID=os.getenv('ECPAY_MERCHANT_ID'),
+            HashKey=os.getenv('ECPAY_HASH_KEY'),
+            HashIV=os.getenv('ECPAY_HASH_IV'),
+        ).ecpay_payment_sdk
+
+        res = request.POST.dict()
+        back_check_mac_value = request.POST.get('CheckMacValue')
+        # order_id = request.POST.get('MerchantTradeNo')
+        return_msg = request.POST.get('RtnMsg')
+        return_code = request.POST.get('RtnCode')
+
+        check_mac_value = ecpay.generate_check_value(res)
+
+        if back_check_mac_value == check_mac_value and return_code == '1':
+            # save to order model
+            return HttpResponse(return_msg)
+
+        return HttpResponse(return_msg)
